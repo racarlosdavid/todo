6239ba9ee206485113f827a8804656b15ae004af
Carlos David Ramirez Altan <carlos.ramirez2@telus.com>
2022-04-05 20:39:50 -0600

    feat: refactor src with outout ejs

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ let content = [];
 let lastId = 0;
 
 // Middlewares
-//app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+//app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
 
 // Templating engine setup
@@ -16,34 +17,32 @@ app.set("view engine", "ejs");
 app.set('views', __dirname + '/views');
 
 // Enpoints
-
 app.get("/", (req, res) => {
   res.status(200).render("index", { content });
 });
 
 app.get("/tasks",(req,res) =>{
-  res.status(200).render("index", { content });
+  res.status(200).send({data: content, error: null})
 })
 
 app.post("/tasks",(req,res) =>{
   lastId++;
-  const {task} = req.body;
+  const {task} = req.body; console.log(req.body)
   const new_task = { id:lastId, status:'todo', task}
   content.push(new_task)
-  res.status(200).redirect('/')
+  //console.log("task added: ",content)
+  res.status(200).send({data: content, error: null})
 })
 
 app.put("/tasks/:id",(req,res) =>{
-  const id = parseInt(req.params.id,10);
+  const id = parseInt(req.params.id,10); 
   const task = content.find(p => p.id == id);
-  //console.log(req.body)
   if (!task) {
-    res.status(404).redirect('/')
-      //res.status(404).json({mensaje: "No existe ninguna task con id "+ id})
+    res.status(404).send({data: content, error: "Task id dont exist"})
   }else{
-      const index = content.indexOf(task);
-      content[index] = {...task,...req.body};
-      res.status(200).redirect('/')
+    const index = content.indexOf(task);
+    content[index] = {...task,...req.body};
+    res.status(200).send({data: content, error: null})
   }
 })
 
@@ -51,14 +50,11 @@ app.delete("/tasks/:id",(req,res) =>{
   //console.log(req.params)
   const id = parseInt(req.params.id,10);
   const task = content.find(p => p.id == id);
-
   if (!task) {
-      //res.status(404).json({mensaje: "No existe ninguna task con id "+ id})
-      res.status(404).redirect('/')
+      res.status(404).send({data: content, error: "Task id dont exist"})
   }else{
       content = content.filter(x => x != task)
-      res.status(200).redirect('/')
-      //res.status(200).json({mensaje: "Task deleted"});
+      res.status(200).send({data: content, error: null})
   }
 })
 
